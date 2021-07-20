cefe5760ac59b3cf85e2cdebff579df594cada45
Filipovici Bogdan <bogdiusf@gmail.com>
2021-07-20 20:13:19 +0300

    first commit

---
+++ src/components/Books.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+
+export default function Books() {
+
+    const [data, setData] = useState([])
+
+    const getData = async () => {
+        const response = await axios.get('/books')
+        const newArr = []
+        response.data.forEach(item => {
+            newArr.push(item)
+        });
+        setData(newArr)
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+    console.log(data)
+    return (
+        data && data.map((item, index) => (
+            <div key={item.id}>Cartea {index+1} - {item.id}</div>
+        ))
+
+    )
+
+}
